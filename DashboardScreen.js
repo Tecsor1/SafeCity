d78ce05747c05b2947d14c1ce80600a7ce0bb1ef
Marco <tecsor.1000@gmail.com>
2023-07-08 00:19:51 -0400

    fotos del usuario

--- DashboardScreen.js
+++ DashboardScreen.js
@@ -1,5 +1,5 @@
 // DashboardScreen.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Button, TextInput, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 import { auth, signOut } from './firebaseConfig';
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
@@ -9,9 +9,23 @@ export default function DashboardScreen({ onOpenCamera, onSearch, setIsImageInfo
   const [searchTerm, setSearchTerm] = useState('');
   const db = getFirestore();
   const [searchResults, setSearchResults] = useState([]);
-
-
-
+  const [viewingUserPhotos, setViewingUserPhotos] = useState(false);
+  const [userPhotos, setUserPhotos] = useState([]);
+  
+  useEffect(() => {
+    if(auth.currentUser) {
+      const fetchUserPhotos = async () => {
+        const q = query(collection(db, "images"), where("userId", "==", auth.currentUser.uid));
+        const querySnapshot = await getDocs(q);
+        let results = [];
+        querySnapshot.forEach((doc) => {
+          results.push(doc.data());
+        });
+        setUserPhotos(results);
+      }
+      fetchUserPhotos();
+    }
+  }, []);
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
@@ -25,44 +39,49 @@ export default function DashboardScreen({ onOpenCamera, onSearch, setIsImageInfo
     setImageSelected(imageData);
     setIsImageInfoOpen(true);
   };
-  
 
- 
-const handleSearch = async () => {
-  console.log('Se presionó el botón de búsqueda');
-  onSearch(searchTerm).then((results) => {
-    setSearchResults(results); // Actualiza el estado con los resultados de la búsqueda
-  });
-};
-  
+  const handleSearch = async () => {
+    console.log('Se presionó el botón de búsqueda');
+    onSearch(searchTerm).then((results) => {
+      setSearchResults(results); // Actualiza el estado con los resultados de la búsqueda
+      setViewingUserPhotos(false); // Reset this state when new search
+    });
+  };
 
-return (
-  <View>
-    <Button title="Open Camera" onPress={onOpenCamera} />
-    <TextInput
-      placeholder="Search"
-      value={searchTerm}
-      onChangeText={setSearchTerm}
-    />
-    <Button title="Search" onPress={handleSearch} />
-    <Button title="Sign Out" onPress={handleSignOut} />
-    
-    <FlatList
-  data={searchResults}
-  keyExtractor={item => item.name}
-  renderItem={({ item }) => (
-    <TouchableOpacity onPress={() => handleImageSelect(item)}>
-      <View style={{ flexDirection: 'row', margin: 10 }}>
-        <Image
-          source={{ uri: item.url }}
-          style={{ width: 50, height: 50 }}
-        />
-        <Text>{item.name}</Text>
-      </View>
-    </TouchableOpacity>
-  )}
-/>
+  const showUserPhotos = () => {
+    setViewingUserPhotos(true); // Show user photos
+  };
 
-  </View>
-);
+  const dataToShow = viewingUserPhotos ? userPhotos : searchResults;
+
+  return (
+    <View>
+      <Button title="Open Camera" onPress={onOpenCamera} />
+      <TextInput
+        placeholder="Search"
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+      />
+      <Button title="Search" onPress={handleSearch} />
+      <Button title="My Photos" onPress={showUserPhotos} />
+      <Button title="Sign Out" onPress={handleSignOut} />
+
+      <FlatList
+        data={dataToShow}
+        keyExtractor={item => item.name}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handleImageSelect(item)}>
+            <View style={{ flexDirection: 'row', margin: 10 }}>
+              <Image
+                source={{ uri: item.url }}
+                style={{ width: 50, height: 50 }}
+              />
+              <Text>{item.name}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+
+    </View>
+  );
 }
